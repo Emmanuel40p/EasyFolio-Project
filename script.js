f191a641757a677c3fc93db5e4afd4a164a1573f
Emmanuel40p <nuelchimezie404@gmail.com>
2025-06-05 21:45:00 +0100

    My first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let menu = document.querySelector('.menuBar')
+let hamburger = document.querySelector('.hamburgerMenu')
+let crossIcon = document.getElementById('cross-icon')
+let icons = document.querySelector('.icons')
+let menu_lists = document.querySelectorAll('menu_lists')
+
+
+// THIS IS THE DROPDOWN MENU BAR -------------------------------------
+
+function dropDownMenu() {
+
+    menu.setAttribute('style', 'display : block')
+    hamburger.setAttribute('style', ' display : none')
+    crossIcon.style.display = 'block'
+
+}
+function removeDdm() {
+    menu.setAttribute('style', 'display : none')
+    hamburger.setAttribute('style', 'display : flex')
+    crossIcon.style.display = 'none'
+
+}
+
+
+function removeMenuOnClickLists() {
+    menu.setAttribute('style', 'display:none')
+    crossIcon.style.display = 'none'
+    hamburger.setAttribute('style', 'display : flex')
+}
+
+
+// function hoverColour(hover) {
+//     menu_lists.forEach
+// }
+
+
